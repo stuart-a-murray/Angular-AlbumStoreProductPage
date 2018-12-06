a75b0e03d659bdf04694df72acb0c9e045ed1483
Stuart Murray <stuart.a.murray@capgemini.com>
2018-12-06 17:41:35 +0000

    added productList component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-
+import { Product } from './product';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
 
@@ -9,6 +9,7 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {
 
@@ -16,6 +17,10 @@ export class ProductService {
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 
 }
